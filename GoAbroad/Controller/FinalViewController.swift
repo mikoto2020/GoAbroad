6b60d61923eabceebc5b846c91756d846c7f7cba
Jeanne <ruborhall@gmail.com>
2020-07-26 03:03:49 +0800

    微調效果

--- GoAbroad/Controller/FinalViewController.swift
+++ GoAbroad/Controller/FinalViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 
 class FinalViewController: UIViewController {
 
+    @IBOutlet weak var waitingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var confirmDetailLabel: UILabel!
+    @IBOutlet weak var confirmLabel: UILabel!
     @IBAction func backToTopBtn(_ sender: UIButton) {
         nextBtnLogic()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        confirmLabel.text = "訂單正在確認中"
+        waitingIndicator.startAnimating()
+        confirmDetailLabel.text = ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        sleep(5)
+        confirmLabel.text = "已收到您的訂單"
+        waitingIndicator.stopAnimating()
+        confirmDetailLabel.text = "火速為您派車中，感謝您的使用"
     }
     
 }
